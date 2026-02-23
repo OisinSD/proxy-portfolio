99f95ebd69f6e9e2b80c4bd195cca19450520cd0
OisinSD <oisin.s.dillon@gmail.com>
2026-02-23 20:23:00 +0000

    PP-63 PP-64 Configured application.properties with the DB URL, username, and password, as well as Verified connection success by running the Java app and checking the logs for HikariPool - Start completed or Hibernate: Table created.

--- pp/src/main/java/proxy/portfolio/pp/TestRepo.java
+++ pp/src/main/java/proxy/portfolio/pp/TestRepo.java
@@ -1,4 +1,6 @@
-package pp;
+package proxy.portfolio.pp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TestRepo extends JpaRepository<TestTable, Long>{
     
--- pp/src/main/java/proxy/portfolio/pp/TestTable.java
+++ pp/src/main/java/proxy/portfolio/pp/TestTable.java
@@ -1,7 +1,7 @@
-package pp;
+package proxy.portfolio.pp;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GenerationType;
+// import jakarta.persistence.GenerationValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -12,7 +12,7 @@ import lombok.Data;
 public class TestTable {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 }
